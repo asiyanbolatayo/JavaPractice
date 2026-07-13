27ed0b459c7500c6b0f5c43984d37bbc7a39e947
Muhammed <40062083+asiyanbolatayo@users.noreply.github.com>
2026-07-13 15:26:32 +0100

    Add files via upload
    Added some Java files

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        byte age = 30;
+        Date now = new Date();
+        System.out.println(now);
+        System.out.println(age);
+        //printPoints(args);
+    } 
+}
